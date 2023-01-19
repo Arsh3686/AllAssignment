2f2e4d9becb2dd7c880bf3bf40734c4908c4c6cb
Arsh3686 <70792511+Arsh3686@users.noreply.github.com>
2023-01-19 12:21:00 +0530

    Update index.js

--- Js-408-01/index.js
+++ Js-408-01/index.js
@@ -2,7 +2,66 @@ let container = document.getElementById("container");
 let id = 1;
 let arr = [];
 let t = 0;
+const month = [
+	{
+		utc: 0,
+		month: " Jan",
+	},
+	{
+		utc: 1,
+		month: " Feb",
+	},
+	{
+		utc: 2,
+		month: " March",
+	},
+	{
+		utc: 3,
+		month: " April",
+	},
+	{
+		utc: 4,
+		month: " May",
+	},
+	{
+		utc: 5,
+		month: " June",
+	},
+	{
+		utc: 6,
+		month: " July",
+	},
+	{
+		utc: 7,
+		month: " Aug",
+	},
+	{
+		utc: 8,
+		month: " Sep",
+	},
+	{
+		utc: 9,
+		month: " Oct",
+	},
+	{
+		utc: 10,
+		month: " Nov",
+	},
+	{
+		utc: 11,
+		month: " Dec",
+	},
+];
+
 function submit() {
+	const date = new Date();
+
+	let datee = `
+	${month[date.getUTCMonth()].month}
+	${date.getDate()}
+	${date.getFullYear()}
+	${date.toLocaleTimeString()}
+	`;
 	let fname = document.getElementById("fname").value;
 	let lname = document.getElementById("lname").value;
 	let country = document.getElementById("country").value;
@@ -15,6 +74,7 @@ function submit() {
 		let obj = {
 			id: id,
 			score: +score,
+			dates: datee,
 			fname: fname,
 			lname: lname,
 			country: country,
@@ -26,10 +86,14 @@ function submit() {
 		div.setAttribute("id", `box${id}`);
 		console.log(fname, lname, country, score);
 		div.innerHTML = `
-                <p>${fname} ${lname}</p>
+                
+				<p>${fname} ${lname}</p>
+				<small class="date">${datee}</small>
+				
                 <p>${country}</p>
                 <p id="upd${id}">${score}</p>
-                <p >${id}</p>
+                
+				
 				<button onclick=del(${id})>✂️</button>
 				<button onclick=decr(${id})>-5</button>
 				<button onclick=incr(${id})>+5</button>
@@ -48,11 +112,16 @@ function decr(b) {
 		return +b.score - +a.score;
 	});
 	// arr[]
-	let temp = +arr[b - 1].score - 5;
+	let find = arr.findIndex((ele) => {
+		return ele.id === b;
+	});
+	console.log("find", find);
+	let temp = +arr[find].score - 5;
+
 	if (temp >= 0) {
-		arr[b - 1].score = +arr[b - 1].score - 5;
+		arr[find].score = +arr[find].score - 5;
 	} else {
-		arr[b - 1].score = 0;
+		arr[find].score = 0;
 	}
 	arr.map((e) => {
 		let div = document.createElement("div");
@@ -62,9 +131,11 @@ function decr(b) {
 
 		div.innerHTML = `
 	            <p>${e.fname} ${e.lname}</p>
+				<small class="date">${e.dates}</small>
 	            <p>${e.country}</p>
 	            <p id="upd${e.id}">${e.score}</p>
-	            <p >${e.id}</p>
+	            
+				
 				<button onclick=del(${e.id})>✂️</button>
 				<button onclick=decr(${e.id})>-5</button>
 				<button onclick=incr(${e.id})>+5</button>
@@ -79,8 +150,11 @@ function incr(b) {
 		id = 1;
 	});
 	console.log(arr);
-
-	arr[b - 1].score = +arr[b - 1].score + 5;
+	let find = arr.findIndex((ele) => {
+		return ele.id === b;
+	});
+	console.log("find", find);
+	arr[find].score = +arr[find].score + 5;
 	arr.sort((a, b) => {
 		return +b.score - +a.score;
 	});
@@ -91,9 +165,10 @@ function incr(b) {
 		div.setAttribute("id", `box${e.id}`);
 		div.innerHTML = `
 	            <p>${e.fname} ${e.lname}</p>
+				<small class="date">${e.dates}</small>
 	            <p>${e.country}</p>
 	            <p id="upd${e.id}">${e.score}</p>
-	            <p >${e.id}</p>
+	            
 				<button onclick=del(${e.id})>✂️</button>
 				<button onclick=decr(${e.id})>-5</button>
 				<button onclick=incr(${e.id})>+5</button>
@@ -101,16 +176,12 @@ function incr(b) {
 		container.appendChild(div);
 		// id++;
 	});
-	// let val = document.getElementById(`upd${b}`);
-	// let temp = val.innerText - 5;
-	// if (temp > 0) {
-	// 	val.innerText = val.innerText - 5;
-	// } else {
-	// 	val.innerText = 0;
-	// }
 }
 function del(e) {
 	document.getElementById(`box${e}`).remove();
+	arr = arr.filter((b) => {
+		return b.id !== e;
+	});
 }
 function check() {
 	let score = document.getElementById("score").value;
@@ -134,4 +205,20 @@ function check() {
 			document.getElementById("score").style.borderColor = "#E5C9C5";
 		}
 	}
+}
+function get(e) {
+	const date = new Date();
+	// console.log(date);
+	console.log(
+		date.toLocaleDateString(),
+		date.getDate(),
+		date.getFullYear(),
+		date.getTime()
+	);
+	let find = arr.findIndex((ele) => {
+		return ele.id === e;
+	});
+	console.log("find", find);
+	console.log("data", document.getElementById(`box${e}`));
+	console.log(arr);
 }
